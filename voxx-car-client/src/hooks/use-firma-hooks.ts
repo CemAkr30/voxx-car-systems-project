b544cc834291aa7a462afd542d737cd9609a3fdd
EBA96 <enes.afsar96@gmail.com>
2025-06-20 16:00:59 +0300

    firma eklendi.

---
+++ voxx-car-client/src/hooks/use-firma-hooks.ts
@@ -0,0 +1,47 @@
+import {createFirma, deleteFirma, getAllFirma, updateFirma,} from "@/requests/firma";
+import type {CreateFirmaRequest, Firma} from "@/schemas/firma";
+import {queryOptions, useMutation, useQueryClient, useSuspenseQuery,} from "@tanstack/react-query";
+
+export const firmalarGetQueryOptions = queryOptions({
+    queryKey: ["firmalar"],
+    queryFn: async () => await getAllFirma(),
+});
+
+export const useFirmalarQuery = () => useSuspenseQuery(firmalarGetQueryOptions);
+
+export const useCreateFirmaMutation = (onSuccess?: () => void) => {
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: async (firma: CreateFirmaRequest): Promise<void> => {
+            await createFirma(firma);
+        },
+        onSuccess() {
+            queryClient.refetchQueries({queryKey: ["firmalar"]});
+            queryClient.invalidateQueries({queryKey: ["firmalar"]});
+            onSuccess?.();
+        },
+    });
+};
+
+export const useUpdateFirmaMutation = (onSuccess?: () => void) => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: async (firma: Firma) => await updateFirma(firma),
+        onSuccess() {
+            queryClient.refetchQueries({queryKey: ["firmalar"]});
+            onSuccess?.();
+        },
+    });
+};
+
+export const useDeleteFirmaMutation = (onSuccess?: () => void) => {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: async (id: string) => await deleteFirma(id),
+        onSuccess() {
+            queryClient.refetchQueries({queryKey: ["firmalar"]});
+            onSuccess?.();
+        },
+    });
+};
