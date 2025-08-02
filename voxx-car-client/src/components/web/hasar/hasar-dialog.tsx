4b6e765496644a3919827edd76629088afd193f8
BulentGuven <bulentguven97@hotmail.com>
2025-08-02 15:16:51 +0300

    fix: fixed build error

--- voxx-car-client/src/components/web/hasar/hasar-dialog.tsx
+++ voxx-car-client/src/components/web/hasar/hasar-dialog.tsx
@@ -88,7 +88,7 @@ export default function HasarDialog(props: HasarDialogProps) {
 							aracFiloId,
 							hasarliParca: value.hasarliParca,
 							hasarTipi: value.hasarTipi,
-							deleted: false,
+							isDeleted: false,
 						},
 					]);
 				} else if (mode === "update") {
