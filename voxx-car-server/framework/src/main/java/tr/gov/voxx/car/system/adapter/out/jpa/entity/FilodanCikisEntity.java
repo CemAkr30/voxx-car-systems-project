33f466b28373487c8b13047f02fcef74a5e034d5
CA <cemakar18@gmail.com>
2025-10-15 12:15:46 +0300

    backend düzeltmeler

--- voxx-car-server/framework/src/main/java/tr/gov/voxx/car/system/adapter/out/jpa/entity/FilodanCikisEntity.java
+++ voxx-car-server/framework/src/main/java/tr/gov/voxx/car/system/adapter/out/jpa/entity/FilodanCikisEntity.java
@@ -1,9 +1,6 @@
 package tr.gov.voxx.car.system.adapter.out.jpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 import tr.gov.voxx.car.system.common.framework.persistence.AbstractEntity;
 import tr.gov.voxx.car.system.domain.enumeration.FilodanCikisNedeni;
@@ -27,5 +24,7 @@ public class FilodanCikisEntity extends AbstractEntity {
     private Double aracDevirGiderleri;
     private String faturaYukle;
     private String aciklama;
+    @Lob
+    private String sozlesme;
 }
 
